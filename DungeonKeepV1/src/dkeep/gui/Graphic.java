64e588f11fa2d4d4ac6355d440145adb72dcdadd
mcarolinaSoares <up201605010@fe.up.pt>
2018-04-06 21:29:30 +0100

    Graphical package

--- DungeonKeepV1/src/dkeep/gui/Graphic.java
+++ DungeonKeepV1/src/dkeep/gui/Graphic.java
@@ -2,22 +2,30 @@ package dkeep.gui;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Graphic extends JPanel {
-	
-	private JFrame frame;
+import dkeep.cli.Commons;
+import dkeep.cli.Map;
+
+public class Graphic extends JPanel implements KeyListener{
+
+	//public JFrame frame;
 	private int width;
 	private int height;
+	private char[][] map;
+	
 	private BufferedImage Menu;
-	private BufferedImage wall;
 	private BufferedImage ground;
+	private BufferedImage chao;
 	private BufferedImage door;
 	private BufferedImage doorOpen;
 	private BufferedImage guard;
@@ -25,20 +33,40 @@ public class Graphic extends JPanel {
 	private BufferedImage weapon;
 	private BufferedImage hero;
 	private BufferedImage key;
+	
+	Image Menudois;
+	Image doors;
+	Image doorsOpens;
+	Image grounds;
+	Image chaos;
+	Image heros;
+	Image keys;
+	Image ogres;
+	Image weapons;
+	Image guards;
+	
 
 	/**
 	 * Create the panel.
 	 */
 	public Graphic() 
 	{
+	
 		super();
+		addKeyListener(this);
+		setFocusable(true);
 	}
 	
-	
+	public void setMap(char[][] map)
+	{
+		this.map = map;
+	}
+
+
 	public void loadImages() throws IOException 
 	{
 		this.Menu = ImageIO.read(new File("images/layout.png"));
-		this.wall = ImageIO.read(new File("images/door.png"));
+		this.chao = ImageIO.read(new File("images/chao.png"));
 		this.ground = ImageIO.read(new File("images/ground.png"));
 		this.door = ImageIO.read(new File("images/door.png"));
 		this.doorOpen = ImageIO.read(new File("images/doorOpen.png"));
@@ -49,19 +77,245 @@ public class Graphic extends JPanel {
 		this.key = ImageIO.read(new File("images/key.png"));
 	}
 
-	public void resizeImage(BufferedImage old)
+	public BufferedImage getMenu() {
+		return this.Menu;
+	}
+
+	public BufferedImage getGround() {
+		return this.ground;
+	}
+
+	public BufferedImage getDoor() {
+		return this.door;
+	}
+
+	public BufferedImage getDoorOpen() {
+		return this.doorOpen;
+	}
+
+	public BufferedImage getGuard() {
+		return this.guard;
+	}
+
+	public BufferedImage getOgre() {
+		return this.ogre;
+	}
+
+	public BufferedImage getWeapon() {
+		return this.weapon;
+	}
+
+	public BufferedImage getHero() {
+		return this.hero;
+	}
+
+	public BufferedImage getKey() {
+		return this.key;
+	}
+
+	public Image resizeImage(BufferedImage old)
 	{
 		Image novo = old;
-		Image newImage = novo.getScaledInstance(50, 50, 50);
+		Image newImage = novo.getScaledInstance(100, 100, 0);
+		return newImage;
 	}
-	
+
+	public void printMap(char[][] map) 
+	{
+		this.map = map;
+		
+		Menudois = Menu.getScaledInstance(805, 633, 0);
+		doors = door.getScaledInstance(70, 70, 0);
+		doorsOpens = doorOpen.getScaledInstance(70, 70, 0);
+		grounds = ground.getScaledInstance(70, 70, 0);
+		chaos = chao.getScaledInstance(70, 70, 0);
+		heros = hero.getScaledInstance(70, 70, 0);
+		keys = key.getScaledInstance(70, 70, 0);
+		ogres = ogre.getScaledInstance(70, 70, 0);
+		weapons = weapon.getScaledInstance(70, 70, 0);
+		guards = guard.getScaledInstance(70, 70, 0);
+		
+		repaint();
+		
+//		Graphics g = null;
+//		super.paintComponent(g);
+//
+//		Image Menudois = Menu.getScaledInstance(805, 633, 0);
+//		
+//		for(int i = 0; i < map.length(); i++)
+//		{	
+//			switch(map.charAt(i))
+//			{
+//			case 'X':
+//				g.drawImage(Menudois, 0, 0, null);
+//				break;
+//			case 'H':
+//				g.drawImage(Menudois, 0, 0, null);
+//				break;
+//			case 'O':
+//				g.drawImage(Menudois, 0, 0, null);
+//				break;
+//			case '*':
+//				g.drawImage(Menudois, 0, 0, null);
+//				break;
+//			case 'K':
+//				g.drawImage(Menudois, 0, 0, null);
+//				break;
+//			case 'S':
+//				g.drawImage(Menudois, 0, 0, null);
+//				break;
+//			case 'I':
+//				g.drawImage(Menudois, 0, 0, null);
+//				break;
+//			default:
+//				g.drawImage(Menudois, 0, 0, null);
+//				break;
+//			}
+//		}
+//
+//		repaint();
+	}
+
+
 	@Override
 	protected void paintComponent(Graphics g) 
 	{
 		super.paintComponent(g);
-		g.drawImage(Menu, 100, 100, null);
+		int x = 0;
+		int y = 0;
+
+		for(int i = 0; i < map.length; i++)
+		{
+			for(int j = 0; j < map[i].length; j++)
+			{
+//				y = (i / 10) * 50;
+//				x = i*50 - (10 * y);
+				x = j*70;
+				y = i*70;
+				switch(map[i][j])
+				{
+				case '\0':
+					break;
+				case 'X':
+					g.drawImage(grounds, x, y, null);
+					break;
+				case 'H':
+					g.drawImage(heros, x, y, null);
+					break;
+				case 'G':
+					g.drawImage(guards, x, y, null);
+					break;
+				case 'O':
+					g.drawImage(ogres, x, y, null);
+					break;
+				case '*':
+					g.drawImage(weapons, x, y, null);
+					break;
+				case 'k':
+					g.drawImage(keys, x, y, null);
+					break;
+				case 'S':
+					g.drawImage(doorsOpens, x, y, null);
+					break;
+				case 'I':
+					g.drawImage(doors, x, y, null); 
+					break;
+				default:
+					g.drawImage(chaos, x, y, null); //
+					break;
+				}
+			}
+		}
+		
+//		for(int i = 0; i < map.length(); i++)
+//		{	
+//			y = (i / 10) * 50;
+//			x = i*50 - (10 * y);
+//			
+//			switch(map.charAt(i))
+//			{
+//			case 'X':
+//				g.drawImage(grounds, x, y, null);
+//				break;
+//			case 'H':
+//				g.drawImage(heros, x, y, null);
+//				break;
+//			case 'G':
+//				g.drawImage(guards, x, y, null);
+//				break;
+//			case 'O':
+//				g.drawImage(ogres, x, y, null);
+//				break;
+//			case '*':
+//				g.drawImage(weapons, x, y, null);
+//				break;
+//			case 'K':
+//				g.drawImage(keys, x, y, null);
+//				break;
+//			case 'S':
+//				g.drawImage(doorsOpens, x, y, null);
+//				break;
+//			case 'I':
+//				g.drawImage(doors, x, y, null); 
+//				break;
+//			default:
+//				g.drawImage(grounds, x, y, null); //
+//				break;
+//			}
+//		}
+//		//g.drawImage(Menudois, 0, 0, null);
 	}
 	
-	
+	public void directionHandler(char dir) {
+		switch(dir) {
+		case 'a':
+			Interfaces.game.moveHero('a');
+			break;
+		case 'w':
+			Interfaces.game.moveHero('w');
+			break;
+		case 's':
+			Interfaces.game.moveHero('s');
+			break;
+		case 'd':
+			Interfaces.game.moveHero('d');
+			break;
+			
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		switch(arg0.getKeyCode()) {
+		case 'a':
+			directionHandler('a');
+			break;
+		case 'w':
+			directionHandler('w');
+			break;
+		case 's':
+			directionHandler('s');
+			break;
+		case 'd':
+			directionHandler('d');
+			break;
+			
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 
 }
--- DungeonKeepV1/src/dkeep/gui/Interfaces.java
+++ DungeonKeepV1/src/dkeep/gui/Interfaces.java
@@ -4,11 +4,14 @@ import java.awt.EventQueue;
 import java.util.*;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 
 import dkeep.cli.Commons;
 import dkeep.cli.Map;
 import dkeep.logic.Game;
+import sun.awt.RequestFocusController;
+
 import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -16,18 +19,24 @@ import java.io.IOException;
 import java.awt.event.ActionEvent;
 import javax.swing.JPanel;
 import java.awt.Color;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Interfaces {
 
 	private JFrame frame;
-	
-	private Game game;
-	
+
+	char[][][] temp;
+
 	private boolean canPrint = false;
-	
-	private Graphic panel = new Graphic();
-	
-	//private Game game;
+
+	private Graphic panel; 
+	private JButton btnUp, btnDown, btnLeft, btnRight;
+
+	static Game game;
+	int levelIndex = 0;
 
 	/**
 	 * Launch the application.
@@ -44,15 +53,22 @@ public class Interfaces {
 			}
 		});
 	}
-	
+
 
 	/**
 	 * Create the application.
 	 * @throws IOException 
 	 */
+
+	private void loadnewMap(char[][] map)
+	{
+		Map level = new Map(map);
+		game = new Game(level);
+	}
+
 	public Interfaces() throws IOException {
 		initialize();
-		
+
 		char [][]map1 = {
 				{'X','X','X','X','X','X','X','X','X','X'},
 				{'X','H',' ',' ','I',' ','X',' ','G','X'},
@@ -64,103 +80,133 @@ public class Interfaces {
 				{'X','X','X',' ','X','X','X','X',' ','X'},
 				{'X',' ','I',' ','I',' ','X','k',' ','X'},
 				{'X','X','X','X','X','X','X','X','X','X'}};
-		
-		Map [][] meusMapas = {
-				
-		};
-		
-		Map level1 = new Map(map1);
-		game = new Game(level1);
-		
+
+		char [][]map2 = {
+				{'X','X','X','X','X','X','X','X','X'},
+				{'I',' ',' ',' ','O',' ',' ','k','X'},
+				{'X',' ',' ',' ',' ',' ',' ',' ','X'},
+				{'X',' ',' ',' ',' ',' ',' ',' ','X'},
+				{'X',' ',' ',' ',' ',' ',' ',' ','X'},
+				{'X',' ',' ',' ',' ',' ',' ',' ','X'},
+				{'X',' ',' ',' ',' ',' ',' ',' ','X'},
+				{'X','H',' ',' ',' ',' ',' ',' ','X'},
+				{'X','X','X','X','X','X','X','X','X'}};
+
+		char[][][] meusMapas = {map1, map2};
+
+		temp = meusMapas;
+		loadnewMap(temp[levelIndex]);
+
 		//meuTexto.setText(game.loadMap(map1, meuTexto));
-		
+
 		JButton btnNewGame = new JButton("New Game");
 		btnNewGame.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				game = new Game(level1);
+				panel.requestFocusInWindow();
 				canPrint = true;
-				panel.repaint();
+				temp = meusMapas;
+				panel.printMap(temp[levelIndex]);
 				//meuTexto.setText(game.loadMap(meuTexto));
+
 			}
 		});
-		
-		btnNewGame.setBounds(595, 693, 141, 29);
+
+		btnNewGame.setBounds(602, 862, 141, 29);
 		frame.getContentPane().add(btnNewGame);
-		
-		JButton btnUp = new JButton("Up");
+
+		btnUp = new JButton("Up");
 		btnUp.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
+				panel.requestFocusInWindow();
+
 				if(canPrint)
 				{
-					game.moveHero('w');
+					panel.directionHandler('w');
+					panel.printMap(game.loadMap(panel));
 					//meuTexto.setText(game.loadMap(meuTexto));
 				}
+				checkState();
+
 			}
 		});
-		btnUp.setBounds(180, 645, 141, 35);
+		btnUp.setBounds(170, 859, 141, 35);
 		frame.getContentPane().add(btnUp);
-		
-		JButton btnLeft = new JButton("Left");
+		btnLeft = new JButton("Left");
 		btnLeft.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
+				panel.requestFocusInWindow();
+
 				if(canPrint) 
 				{
-					game.moveHero('e');
+					panel.directionHandler('a');
+					panel.printMap(game.loadMap(panel));
 					//meuTexto.setText(game.loadMap(meuTexto));
 				}
+				checkState();
 			}
 		});
-		btnLeft.setBounds(49, 701, 141, 35);
+		btnLeft.setBounds(39, 915, 141, 35);
 		frame.getContentPane().add(btnLeft);
-		
-		JButton btnRight = new JButton("Right");
+
+		btnRight = new JButton("Right");
 		btnRight.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				panel.requestFocusInWindow();
+
 				if(canPrint) 
 				{
-					game.moveHero('d');
+					panel.directionHandler('d');
+					panel.printMap(game.loadMap(panel));
 					//meuTexto.setText(game.loadMap(meuTexto));
 				}
+				checkState();
 			}
 		});
-		btnRight.setBounds(297, 701, 141, 35);
+		btnRight.setBounds(287, 915, 141, 35);
 		frame.getContentPane().add(btnRight);
-		
-		JButton btnDown = new JButton("Down");
+
+		btnDown = new JButton("Down");
 		btnDown.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				panel.requestFocusInWindow();
+
 				if(canPrint) 
 				{
-					game.moveHero('s');
+					panel.directionHandler('s');
+					panel.printMap(game.loadMap(panel));
 					//meuTexto.setText(game.loadMap(meuTexto));
 				}
+				checkState();
 			}
 		});
-		btnDown.setBounds(180, 755, 141, 35);
+		btnDown.setBounds(170, 969, 141, 35);
 		frame.getContentPane().add(btnDown);
-		
-		
+
+
 		JButton btnExit = new JButton("Exit");
 		btnExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				panel.requestFocusInWindow();
 				System.exit(0);
 			}
 		});
-		btnExit.setBounds(614, 762, 106, 21);
+		btnExit.setBounds(621, 931, 106, 21);
 		frame.getContentPane().add(btnExit);
-		
-		
-		
-		panel.setBackground(Color.RED);
-//		Graphic panel = new Graphic();
-		panel.setBounds(0, 0, 805, 633);
+
+
+		panel = new Graphic();
+		panel.setBackground(Color.BLACK);
+		//		Graphic panel = new Graphic();
+		panel.setBounds(0, 0, 700, 700);
 		panel.loadImages();
 		frame.getContentPane().add(panel);
-		
+
+
+		panel.setMap(temp[levelIndex]);
+		frame.getContentPane().add(panel);
+
+		//JOptionPane.showMessageDialog(frame, "Eggs are not supposed to be green.");
 	}
 
 	/**
@@ -168,8 +214,29 @@ public class Interfaces {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 831, 882);
+		frame.setBounds(100, 100, 900, 1076);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		frame.setResizable(false);
 	}
+
+	public void checkState() 
+	{
+		if(game.isGameOver())
+		{
+			JOptionPane.showMessageDialog(null, "GAME OVER");
+			btnDown.setEnabled(false);
+			btnUp.setEnabled(false);
+			btnLeft.setEnabled(false);
+			btnRight.setEnabled(false);
+		}
+		else if(game.isWon())
+		{
+			JOptionPane.showMessageDialog(null, "LEVEL 1 COMPLETE!");
+			levelIndex++;
+			loadnewMap(temp[levelIndex]);
+			panel.printMap(temp[levelIndex]);
+		}
+	}
+
 }
